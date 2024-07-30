c215057cc36cb51b7790c14786a58fe56edf75fb
Divyeshakti <divye.m@zeotap.com>
2024-07-30 13:17:47 +0530

    Adding some more helpful testing panels

--- js/snippet.interact.js
+++ js/snippet.interact.js
@@ -1,16 +1,82 @@
+function getQueryParams() {
+  var queryParams = {};
+  var queryString = window.location.search.substring(1);
+  var params = queryString.split("&");
+  for (var i = 0; i < params.length; i++) {
+    var param = params[i].split("=");
+    var key = decodeURIComponent(param[0]);
+    var value = decodeURIComponent(param[1]);
+    queryParams[key] = value;
+  }
+  return queryParams;
+}
+
+function setQueryParams(params) {
+  var queryString = "";
+  for (var key in params) {
+    if (params.hasOwnProperty(key)) {
+      var value = encodeURIComponent(params[key]);
+      queryString += key + "=" + value + "&";
+    }
+  }
+  queryString = queryString.slice(0, -1); // Remove the trailing '&'
+  window.location.search = queryString;
+}
+
+function getSessionStorage() {
+  var sessionStorage = window.sessionStorage;
+  var sessionData = sessionStorage.getItem("zeotapInteract") || "{}";
+  try {
+    return JSON.parse(sessionData);
+  } catch (error) {
+    console.error("Error parsing session data:", error);
+    return {};
+  }
+}
+
+function setSessionStorage(data) {
+  var sessionStorage = window.sessionStorage;
+  sessionStorage.setItem("zeotapInteract", JSON.stringify(data));
+}
+
+function getWriteKey(env = "qa") {
+  return {
+    qa: "39e82d18-0151-4ba3-84d1-9e3dba037917",
+    staging: "5d8ce731-f41d-44a7-a9e8-9a6713b67404",
+  }[env];
+}
+
 (function (win, doc) {
   var zs = doc.createElement("script");
   zs.type = "text/javascript";
   // zs.integrity = '';
   zs.crossorigin = "anonymous";
   zs.async = true; // the script is loaded asynchronously
+  var params = getQueryParams();
+
+  var storage = getSessionStorage();
+  if (params["env"]) {
+    storage["env"] = params["env"];
+    setSessionStorage(storage);
+  } else {
+    if (storage["env"]) {
+      setQueryParams({ env: storage["env"] });
+    }
+  }
+
+  if (storage["env"] === "staging") {
+    zs.src = "../js/staging/interact.min.js";
+  } else {
+    zs.src = "../js/interact.min.js";
+  }
+
   //   zs.src = "http://localhost:8081/interact.min.js";
-  zs.src = "../js/interact.min.js";
+
   zs.onload = function () {};
 
   var s = doc.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(zs, s);
-  var funcs = ["init", "setUserIdentities"];
+  var funcs = ["init", "setUserIdentities", "getUserIdentities"];
   function setUpProxy(instance, functions, queue) {
     function proxyMain(fn) {
       instance[fn] = function () {
@@ -27,5 +93,6 @@
   var zeotapInteract = win.zeotapInteract || { _q: [] };
   setUpProxy(zeotapInteract, funcs, "_q");
   win.zeotapInteract = zeotapInteract;
+  const writeKey = getWriteKey(storage["env"] || "qa");
+  window.zeotapInteract.init(writeKey);
 })(window, document);
-window.zeotapInteract.init("39e82d18-0151-4ba3-84d1-9e3dba037917");
--- js/test.js
+++ js/test.js
@@ -1,12 +1,12 @@
-window.addEventListener("sendGAMessage", function ($event) {
-  console.log("event.data: ", JSON.stringify($event.detail));
-  var resp = JSON.stringify($event.detail);
-  const div = document.querySelector("#gamevents");
-  const elem = document.createElement("p");
-  elem.textContent = resp;
-  div.appendChild(elem);
-  //   div.textContent = resp;
-});
+// window.addEventListener("sendGAMessage", function ($event) {
+//   console.log("event.data: ", JSON.stringify($event.detail));
+//   var resp = JSON.stringify($event.detail);
+//   const div = document.querySelector("#gamevents");
+//   const elem = document.createElement("p");
+//   elem.textContent = resp;
+//   div.appendChild(elem);
+//   //   div.textContent = resp;
+// });
 
 window.zGAMCallback = function (data) {
   console.log("zGAMCallback: ", JSON.stringify(data));
@@ -18,9 +18,51 @@ window.zGAMCallback = function (data) {
   //   div.textContent = resp;
 };
 
+document.querySelector("#submit").addEventListener("click", function () {
+  var fieldName = document.querySelector("#fieldName").value;
+  var fieldValue = document.querySelector("#fieldValue").value;
+  window.zeotapInteract.setUserIdentities({ [fieldName]: fieldValue });
+  displayIdentities();
+
+  // window.location.href = "/store";
+});
+
+document
+  .querySelector("#resetIdentities")
+  .addEventListener("click", function () {
+    displayIdentities();
+    // window.location.href = "/store";
+  });
+
 window.addEventListener("DOMContentLoaded", () => {
   console.log("DOMContentLoaded event fired");
+  displayIdentities();
+
   // window.zeotapInteract.setUserIdentities({
   //   email: "divye.m@zeotap.com",
   // });
 });
+
+function displayIdentities() {
+  const identities = window.zeotapInteract
+    ? window.zeotapInteract.getUserIdentities()
+    : {};
+  const div = document.querySelector("#identities");
+  if (div) {
+    div.innerHTML = ""; // Clear previous contents
+    for (const key in identities) {
+      const elem = document.createElement("p");
+      elem.textContent = `${key}: ${identities[key]}`;
+      div.appendChild(elem);
+    }
+  }
+  // div.innerHTML = ""; // Clear previous contents
+  // const value = localStorage.getItem("");
+  // for (let i = 0; i < localStorage.length; i++) {
+  //   const key = localStorage.key(i);
+  //   const value = localStorage.getItem(key);
+  //   const elem = document.createElement("p");
+  //   elem.textContent = `${key}: ${value}`;
+  //   div.appendChild(elem);
+  // }
+}
